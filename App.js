51428622a4ad402ca96af5e0f694ef30a7923d22
mucahit-sahin <mucahitsahin786@gmail.com>
2020-11-03 17:26:42 +0300

    ilk commit

---
+++ App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import {SafeAreaView, StyleSheet, View, Text, StatusBar} from 'react-native';
+
+const App = () => {
+  return (
+    <>
+      <StatusBar barStyle="dark-content" />
+      <SafeAreaView>
+        <View>
+          <Text>Getirt</Text>
+        </View>
+      </SafeAreaView>
+    </>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default App;
---
+++ Readme.md
@@ -0,0 +1,3 @@
+# Getirt Mobil Uygulama
+
+İnsanların market, manav gibi alışverişlerini evden çıkmadan başkalarına belli bir komisyonla yaptırmasını sağlayan bir mobil uygulama projesidir.
