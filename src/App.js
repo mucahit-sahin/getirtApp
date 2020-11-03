a41cd4457afc2704a0bac158cefb72c57de99dac
mucahit-sahin <mucahitsahin786@gmail.com>
2020-11-03 17:42:17 +0300

    klasor düzeni ayarlandı

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import {SafeAreaView, StyleSheet, View, Text, StatusBar} from 'react-native';
+import Login from './Screens/Login';
 
 const App = () => {
   return (
     <>
       <StatusBar barStyle="dark-content" />
       <SafeAreaView>
-        <View>
-          <Text>Getirt</Text>
-        </View>
+        <Login />
       </SafeAreaView>
     </>
   );
