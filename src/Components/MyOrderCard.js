c62727aa4bc21306cdc1125dbbf4f3830096ad74
mucahit-sahin <mucahitsahin786@gmail.com>
2020-12-11 13:20:18 +0300

    MyOrderCard componenti oluşturuldu

--- src/Components/MyOrderCard.js
+++ src/Components/MyOrderCard.js
@@ -2,9 +2,15 @@ import React from 'react';
 import {Image, StyleSheet, Text, View, TouchableOpacity} from 'react-native';
 import Colors from '../Utils/Colors';
 
-const MyOrderCard = ({username, userImage, orderWeight, orderPrice}) => {
+const MyOrderCard = ({
+  username,
+  userImage,
+  orderWeight,
+  orderPrice,
+  onPress,
+}) => {
   return (
-    <TouchableOpacity style={styles.container}>
+    <TouchableOpacity style={styles.container} onPress={onPress}>
       <View style={styles.header}>
         <Image
           style={styles.profileImage}
