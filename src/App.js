ec5c715bf8981b506356fc54dea77824f8ebb82e
mucahit-sahin <mucahitsahin786@gmail.com>
2020-12-12 20:29:24 +0300

    Üye olma ve  giriş yapma işlemleri için gerekli auth işlemleri yapıldı

--- src/App.js
+++ src/App.js
@@ -1,19 +1,14 @@
 import React from 'react';
 import {SafeAreaView, StyleSheet, View, Text, StatusBar} from 'react-native';
-import BottomNavigation from './Navigations/BottomNavigation';
-import SignNavigation from './Navigations/SignNavigation';
+import Providers from './Navigations/index';
 
 const App = () => {
   return (
     <>
       <StatusBar barStyle="dark-content" />
-      {/* <Login /> */}
-      {/*<SignNavigation />*/}
-      <BottomNavigation />
+
+      <Providers />
     </>
   );
 };
-
-const styles = StyleSheet.create({});
-
 export default App;
