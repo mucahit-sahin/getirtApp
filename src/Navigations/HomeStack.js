5316fdf8e086fece8a5ba9682dcb53ce0bfbed3d
mucahit-sahin <mucahitsahin786@gmail.com>
2020-12-18 02:16:32 +0300

    Profilini tamamlama ekranı ve tabBar gizleme yapıldı

--- src/Navigations/HomeStack.js
+++ src/Navigations/HomeStack.js
@@ -6,15 +6,26 @@ import CreateShoopingCart from '../Screens/CreateShoopingCart';
 import Orders from '../Screens/Orders';
 import ConfirmOrder from '../Screens/ConfirmOrder';
 import OrderDetails from '../Screens/OrderDetails';
+import UserDetailsForm from '../Screens/UserDetailsForm';
 
 const HomeStack = createStackNavigator();
 
-function HomeStackScreen() {
+function HomeStackScreen({navigation, route}) {
+  if (route.state && route.state.index > 0) {
+    navigation.setOptions({tabBarVisible: false});
+  } else {
+    navigation.setOptions({tabBarVisible: true});
+  }
   return (
     <HomeStack.Navigator>
       <HomeStack.Screen
         name="Home"
         component={Home}
+        options={{headerShown: false}}
+      />
+      <HomeStack.Screen
+        name="UserDetailsForm"
+        component={UserDetailsForm}
         options={{headerShown: false}}
       />
       <HomeStack.Screen
